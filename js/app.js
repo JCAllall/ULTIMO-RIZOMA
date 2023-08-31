e954c181e021137d607c5b5ff440e2eeb921b824
Juan Cruz Allall <allalljuancruz@gmail.com>
2023-08-31 04:05:13 -0300

    logo

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const carrito = document.querySelector("#carrito");
 const listaProductos = document.querySelector("#lista-productos");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
-const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
+const vaciarCarritoBtn = document.querySelector(".vaciar-carrito");
 let articulosCarrito = [];
 
 // Listeners
@@ -27,7 +27,7 @@ function agregarProducto(e) {
   if (e.target.classList.contains("btn-agregar")) {
     const producto = e.target.parentElement.parentElement;
     // Enviamos el curso seleccionado para tomar sus datos
-    leerDatosProductos(producto);
+    leerDatosProducto(producto);
   }
 }
 
@@ -35,19 +35,23 @@ function agregarProducto(e) {
 function leerDatosProducto(producto) {
   const infoProducto = {
     imagen: producto.querySelector("img").src,
-    titulo: producto.querySelector("h4").textContent,
+    titulo: producto.querySelector("h3").textContent,
     precio: producto.querySelector(".precio span").textContent,
     id: producto.querySelector("a").getAttribute("data-id"),
     cantidad: 1,
   };
 
-  if (articulosCarrito.some((producto) => producto.id === infoProducto.id)) {
-    const productos = articulosCarrito.map((producto) => {
+
+  // Revisa si un elemento ya existe
+  const existe = articulosCarrito.some( producto => producto.id === infoProducto.id);
+  if (existe) {
+    // Actualizamos la cantidad
+    const productos = articulosCarrito.map( producto => {
       if (producto.id === infoProducto.id) {
         producto.cantidad++;
-        return producto;
+        return producto; // retorna el objeto actualizado
       } else {
-        return producto;
+        return producto; // retorna un producto nuevo
       }
     });
     articulosCarrito = [...productos];
@@ -77,21 +81,20 @@ function eliminarProducto(e) {
   }
 }
 
-// Muestra el curso seleccionado en el Carrito
+// Muestra el producto seleccionado en el Carrito
 function carritoHTML() {
   vaciarCarrito();
 
-  articulosCarrito.forEach((producto) => {
+  articulosCarrito.forEach( producto => {
+    const {imagen, titulo, precio, cantidad, id } = producto;
     const row = document.createElement("tr");
     row.innerHTML = `
-               <td>  
-                    <img src="${curso.imagen}" width=100>
-               </td>
-               <td>${curso.titulo}</td>
-               <td>${curso.precio}</td>
-               <td>${curso.cantidad} </td>
+               <td><img src="${imagen}" width=100></td>
+               <td>${titulo}</td>
+               <td>${precio}</td>
+               <td>${cantidad} </td>
                <td>
-                    <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+                    <a href="#" class="borrar-producto" data-id="${id}"> X </a>
                </td>
           `;
     contenedorCarrito.appendChild(row);
